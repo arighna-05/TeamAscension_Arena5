0bc9dabc12302fa6cb9e1c910c4ddbb25a761acf
Mr.Invictus <arnabmandal261@gmail.com>
2026-04-24 17:56:40 +0530

    Update Firebase config and enable authorized domain

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // TODO: Replace with your actual Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBnxgEHR-D7YYm-nKyt8qBOlvr2cz5QHWQ",
-  authDomain: "agrilink-app-9d4fb.firebaseapp.com",
-  projectId: "agrilink-app-9d4fb",
-  storageBucket: "agrilink-app-9d4fb.firebasestorage.app",
-  messagingSenderId: "778261653433",
-  appId: "1:778261653433:web:f4b8d3409d16cff909f868",
-  measurementId: "G-XKPTMKH92G"
+  apiKey: "AIzaSyB68U08bNUt9UlDKZvTW53dgx-JyYvfP4s",
+  authDomain: "agrilink-deployment.firebaseapp.com",
+  projectId: "agrilink-deployment",
+  storageBucket: "agrilink-deployment.firebasestorage.app",
+  messagingSenderId: "1007438454762",
+  appId: "1:1007438454762:web:55030e71bf21c9021a9c93",
+  measurementId: "G-Y45TCBE0DS"
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
